1c1ac4f5ba20ccd2075f9f746ae8f1adf5e5dc41
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-08 02:05:34 +0300

    data.js file is created

---
+++ src/data/data.js
@@ -0,0 +1,168 @@
+export const data = {
+  tr: {
+    header: {
+      darkmode: "KARANLIK MOD",
+      lightmode: "AYDINLIK MOD",
+      language: "SWITCH TO ENGLISH",
+    },
+    introduction: {
+      greeting: "Merhaba! 👋",
+      p1: "Ben Melis. Full-stack geliştiriciyim. Sağlam ve ölçeklenebilir ön yüz web sayfaları oluşturabilirim. Haydi tanışalım!",
+      p2: "Şu an UX, UI ve Web Tasarım Projeleri için serbest çalışıyorum.",
+      p3: "Takımınıza davet edin ",
+      p4: " hacermelisyilmaz@gmail.com",
+    },
+    skills: {
+      title: "Beceriler",
+      js: "JAVASCRIPT",
+      react: "REACT",
+      redux: "REDUX",
+      node: "NODE",
+      vs: "VS CODE",
+      figma: "FIGMA",
+    },
+    profile: {
+      title: "Profil",
+      basicinfo: {
+        title: "Temel Bilgiler",
+        birthdatekey: "Doğum Tarihi",
+        birthdate: "23.06.1999",
+        locationkey: "İkamet Şehri",
+        location: "İstanbul",
+        edu: "Eğitim Durumu",
+        edu: "Boğaziçi Üniversitesi, Mühendislik Fakültesi, 2018",
+        rolekey: "Tercih Ettiği Rol",
+        role: "Frontend, UI",
+      },
+      aboutme: {
+        title: "Hakkımda",
+        p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Veniam aut poil laborum oliquam voluptatum nisi mollitia.",
+        p2: "Mnima accusomus ratione solula aperiam sit voluptate? Dicto quod deserunt quam temporibus cumque magnam!",
+      },
+    },
+    projects: {
+      title: "Projeler",
+      randjokes: {
+        title: "Şakalar",
+        p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Veniam aut poil laborum oliquam voluptatum nisi mollitia.",
+        tags: {
+          react: "react",
+          vercel: "vercel",
+          axios: "axios",
+          router: "router",
+        },
+        links: {
+          git: "Github'da göster",
+          redirect: "Uygulamaya git ",
+        },
+      },
+      bored: {
+        title: "Sıkıldınız mı?",
+        p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Veniam aut poil laborum oliquam voluptatum nisi mollitia. Mnima accusomus ratione solula aperiam sit voluptate? Dicto quod deserunt quam temporibus cumque magnam!",
+        tags: {
+          react: "react",
+          redux: "redux",
+          axios: "axios",
+          router: "router",
+          vercel: "vercel",
+        },
+        links: {
+          git: "Github'da göster",
+          redirect: "Uygulamaya git ",
+        },
+      },
+    },
+    footer: {
+      title: "Bir sonraki siteniz için birlikte çalışalım.",
+      links: {
+        git: "Github",
+        blog: "Kişisel Blog",
+        linkedin: "Linkedin",
+        email: "Email",
+      },
+    },
+  },
+  eng: {
+    header: {
+      darkmode: "DARK MODE",
+      lightmode: "LIGHT MODE",
+      language: "TÜRKÇE'YE GEÇ",
+    },
+    introduction: {
+      greeting: "Hi! 👋",
+      p1: "I'm Melis. I'm a full-stack developer. I can craft solid and scalable frontend products. Let's meet!",
+      p2: "Currently Freelancing for UX, UI, & Web Design Project.",
+      p3: "Invite me to join your team ",
+      p4: " hacermelisyilmaz@gmail.com",
+    },
+    skills: {
+      title: "Skills",
+      js: "JAVASCRIPT",
+      react: "REACT",
+      redux: "REDUX",
+      node: "NODE",
+      vs: "VS CODE",
+      figma: "FIGMA",
+    },
+    profile: {
+      title: "Profile",
+      basicinfo: {
+        title: "Basic Information",
+        birthdatekey: "Date of Birth",
+        birthdate: "23.06.1999",
+        locationkey: "City of Residence",
+        location: "İstanbul",
+        edu: "Education",
+        edu: "Boğaziçi Üniversitesi, Mühendislik Fakültesi, 2018",
+        rolekey: "Preferred Role",
+        role: "Frontend, UI",
+      },
+      aboutme: {
+        title: "About me",
+        p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Veniam aut poil laborum oliquam voluptatum nisi mollitia.",
+        p2: "Mnima accusomus ratione solula aperiam sit voluptate? Dicto quod deserunt quam temporibus cumque magnam!",
+      },
+    },
+    projects: {
+      title: "Projects",
+      randjokes: {
+        title: "Random Jokes",
+        p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Veniam aut poil laborum oliquam voluptatum nisi mollitia.",
+        tags: {
+          react: "react",
+          vercel: "vercel",
+          axios: "axios",
+          router: "router",
+        },
+        links: {
+          git: "View on Github",
+          redirect: "Go to app ",
+        },
+      },
+      bored: {
+        title: "Are you bored?",
+        p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit Veniam aut poil laborum oliquam voluptatum nisi mollitia. Mnima accusomus ratione solula aperiam sit voluptate? Dicto quod deserunt quam temporibus cumque magnam!",
+        tags: {
+          react: "react",
+          redux: "redux",
+          axios: "axios",
+          router: "router",
+          vercel: "vercel",
+        },
+        links: {
+          git: "View on Github",
+          redirect: "Go to app ",
+        },
+      },
+    },
+    footer: {
+      title: "Let's work together on your next product.",
+      links: {
+        git: "Github",
+        blog: "Personal Blog",
+        linkedin: "Linkedin",
+        email: "Email",
+      },
+    },
+  },
+};
