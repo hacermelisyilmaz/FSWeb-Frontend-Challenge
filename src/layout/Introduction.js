8cb5eaf44341d61d1a838c26b122fe285e8cb10d
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-08 21:14:54 +0300

    Introduction component is created

--- src/layout/Introduction.js
+++ src/layout/Introduction.js
@@ -1,5 +1,29 @@
+import { useContext } from "react";
+import { TextContext } from "../context/TextProvider";
+import { ModeLangContext } from "../context/ModeLangProvider";
+
 function Introduction() {
-  return <div className="Introduction"></div>;
+  const textData = useContext(TextContext);
+  const { lang } = useContext(ModeLangContext);
+
+  const componentTexts = textData[lang].introduction;
+
+  return (
+    <div className="Introduction">
+      <h2>{componentTexts.greeting}</h2>
+      <div>
+        <h1>{componentTexts.p1}</h1>
+        <h1>{componentTexts.p2}</h1>
+      </div>
+      {/* ikonlar buraya */}
+      <p>{componentTexts.p3}</p>
+      <p>
+        <p>{componentTexts.p4}</p>
+        {/* arrow ikonu */}
+        <p>{componentTexts.p5}</p>
+      </p>
+    </div>
+  );
 }
 
 export default Introduction;
