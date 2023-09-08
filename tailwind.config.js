bdb7dd9b3313311b1213083dd4e683a54845a4f8
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-08 21:09:49 +0300

    Colors are defined in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: "#E92577",
+      lightblue: "#82BBFF",
+      lightgray: "#F4F4F4",
+      gray: "#D9D9D9",
+      darkgray: "#525252",
+      black: "#0A0A14",
+      lightpink: "#EA2678",
+      pastelblue: "#DDEEFE",
+      pastelgreen: "#D9F6F1",
+      cerulean: "#1769FF",
+      blue: "#0077B5",
+      redpink: "#B92B5F",
+      reduxpurple: "#764ABC",
+    },
     extend: {},
   },
   plugins: [],
