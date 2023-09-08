b593f9ecb85bf430fdaaec85305006f080dbc15e
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-08 21:11:51 +0300

    Layout components are added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./layout/Header";
+import Introduction from "./layout/Introduction";
+import Skills from "./layout/Skills";
+import Profile from "./layout/Profile";
+import Projects from "./layout/Projects";
+import Footer from "./layout/Footer";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Introduction />
+      <Skills />
+      <Profile />
+      <Projects />
+      <Footer />
     </div>
   );
 }
