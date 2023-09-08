46f54001e0cf2b5e9dafc7638df4f2c1896797a3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-08 21:15:33 +0300

    Skills component is created

--- src/layout/Skills.js
+++ src/layout/Skills.js
@@ -1,5 +1,29 @@
+import { useContext } from "react";
+import { TextContext } from "../context/TextProvider";
+import { ModeLangContext } from "../context/ModeLangProvider";
+
 function Skills() {
-  return <div className="Skills"></div>;
+  const textData = useContext(TextContext);
+  const { lang } = useContext(ModeLangContext);
+
+  const componentTexts = textData[lang].skills;
+  const skills = componentTexts.skillicons;
+
+  return (
+    <div className="Skills">
+      <h2>{componentTexts.title} </h2>
+      <div id="skill-icons">
+        {skills.map((skill) => {
+          return (
+            <div className="skill-icon">
+              <img src={skill.url} alt={skill.name} />
+              <p>{skill.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
