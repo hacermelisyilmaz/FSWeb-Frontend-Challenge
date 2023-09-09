0cc504124fc6ddd168ba024e2c79f63af56a146c
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-09 16:18:19 +0300

    Footer component is created

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import { TextContext } from "../context/TextProvider";
+import { ModeLangContext } from "../context/ModeLangProvider";
+
 function Footer() {
-  return <div className="Footer"></div>;
+  const textData = useContext(TextContext);
+  const { lang } = useContext(ModeLangContext);
+
+  const componentTexts = textData[lang].footer;
+
+  return (
+    <div className="Footer">
+      <h1>{componentTexts.title}</h1>
+      <div id="footer-links">
+        <a>{componentTexts.links.git} </a>
+        <a>{componentTexts.links.blog} </a>
+        <a>{componentTexts.links.linkedin} </a>
+        <a>{componentTexts.links.email} </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
