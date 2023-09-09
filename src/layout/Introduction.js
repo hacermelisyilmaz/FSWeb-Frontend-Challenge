b97cd2cb527b3d249765162b3a7102bb4128afae
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-09 16:20:14 +0300

    Image is added. Tags are corrected

--- src/layout/Introduction.js
+++ src/layout/Introduction.js
@@ -10,18 +10,25 @@ function Introduction() {
 
   return (
     <div className="Introduction">
-      <h2>{componentTexts.greeting}</h2>
-      <div>
-        <h1>{componentTexts.p1}</h1>
-        <h1>{componentTexts.p2}</h1>
+      <div id="intro-left">
+        <div id="greeting">
+          <h2>{componentTexts.greeting}</h2>
+          <div>
+            <h1>{componentTexts.p1}</h1>
+            <h1>{componentTexts.p2}</h1>
+          </div>
+        </div>
+        <div id="info-footer">
+          {/* ikonlar buraya */}
+          <p>{componentTexts.p3}</p>
+          <div>
+            <p>{componentTexts.p4}</p>
+            {/* arrow ikonu */}
+            <p>{componentTexts.p5}</p>
+          </div>
+        </div>
       </div>
-      {/* ikonlar buraya */}
-      <p>{componentTexts.p3}</p>
-      <p>
-        <p>{componentTexts.p4}</p>
-        {/* arrow ikonu */}
-        <p>{componentTexts.p5}</p>
-      </p>
+      <img src={componentTexts.image} />
     </div>
   );
 }
