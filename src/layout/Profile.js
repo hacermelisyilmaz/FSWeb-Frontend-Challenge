a9f84c0bf4bf19dcdf6f9274b97d8203be401446
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-10 11:24:49 +0300

    Background colors are added

--- src/layout/Profile.js
+++ src/layout/Profile.js
@@ -11,31 +11,31 @@ function Profile() {
   const aboutTexts = componentTexts.aboutme;
 
   return (
-    <div className="Profile">
+    <div className="Profile component bg-bggray">
       <h2>{componentTexts.title}</h2>
-      <div id="basic-info">
-        <h3>{infoTexts.title}</h3>
-        <div>
-          <p className="info-key">{infoTexts.birthdatekey} </p>
-          <p className="info-value">{infoTexts.birthdate} </p>
+      <div className="w-3/4 mx-auto flex">
+        <div id="basic-info" className="bg-white">
+          <h3>{infoTexts.title}</h3>
+          <div id="info-grid" className="grid grid-cols-2 gap-y-5">
+            <p className="info-key">{infoTexts.birthdatekey} </p>
+            <p className="info-value">{infoTexts.birthdate} </p>
+
+            <p className="info-key">{infoTexts.locationkey} </p>
+            <p className="info-value">{infoTexts.location} </p>
+
+            <p className="info-key">{infoTexts.edukey} </p>
+            <p className="info-value">{infoTexts.edu} </p>
+
+            <p className="info-key">{infoTexts.rolekey} </p>
+            <p className="info-value">{infoTexts.role} </p>
+          </div>
         </div>
-        <div>
-          <p className="info-key">{infoTexts.locationkey} </p>
-          <p className="info-value">{infoTexts.location} </p>
+        >
+        <div className="about-me">
+          <h3>{aboutTexts.title}</h3>
+          <p>{aboutTexts.p1} </p>
+          <p>{aboutTexts.p2} </p>
         </div>
-        <div>
-          <p className="info-key">{infoTexts.edukey} </p>
-          <p className="info-value">{infoTexts.edu} </p>
-        </div>
-        <div>
-          <p className="info-key">{infoTexts.rolekey} </p>
-          <p className="info-value">{infoTexts.role} </p>
-        </div>
-      </div>
-      <div className="about-me">
-        <h3>{aboutTexts.title}</h3>
-        <p>{aboutTexts.p1} </p>
-        <p>{aboutTexts.p2} </p>
       </div>
     </div>
   );
