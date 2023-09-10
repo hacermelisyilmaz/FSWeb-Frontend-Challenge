65dfc57c16fd7d727060c3ae7d10c87bb1f17d7b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-10 11:24:00 +0300

    Footer contents are wrapped with another div

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -9,13 +9,15 @@ function Footer() {
   const componentTexts = textData[lang].footer;
 
   return (
-    <div className="Footer">
-      <h1>{componentTexts.title}</h1>
-      <div id="footer-links">
-        <a>{componentTexts.links.git} </a>
-        <a>{componentTexts.links.blog} </a>
-        <a>{componentTexts.links.linkedin} </a>
-        <a>{componentTexts.links.email} </a>
+    <div className="Footer component">
+      <div className="w-4/5 mx-auto">
+        <h1>{componentTexts.title}</h1>
+        <div id="footer-links">
+          <a>{componentTexts.links.git} </a>
+          <a>{componentTexts.links.blog} </a>
+          <a>{componentTexts.links.linkedin} </a>
+          <a>{componentTexts.links.email} </a>
+        </div>
       </div>
     </div>
   );
