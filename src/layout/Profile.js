6800ae86f6d43ddc7657513b5b344d375de474f9
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-10 16:56:23 +0300

    Colors & gap are added

--- src/layout/Profile.js
+++ src/layout/Profile.js
@@ -13,28 +13,28 @@ function Profile() {
   return (
     <div className="Profile component bg-bggray">
       <h2>{componentTexts.title}</h2>
-      <div className="w-3/4 mx-auto flex">
+      <div className="w-4/5 mx-auto flex gap-7">
         <div id="basic-info" className="bg-white">
-          <h3>{infoTexts.title}</h3>
+          <h3 className="text-headerlightpink">{infoTexts.title}</h3>
           <div id="info-grid" className="grid grid-cols-2 gap-y-5">
-            <p className="info-key">{infoTexts.birthdatekey} </p>
-            <p className="info-value">{infoTexts.birthdate} </p>
+            <p className="info-key">{infoTexts.birthdatekey}</p>
+            <p className="info-value">{infoTexts.birthdate}</p>
 
-            <p className="info-key">{infoTexts.locationkey} </p>
-            <p className="info-value">{infoTexts.location} </p>
+            <p className="info-key">{infoTexts.locationkey}</p>
+            <p className="info-value">{infoTexts.location}</p>
 
-            <p className="info-key">{infoTexts.edukey} </p>
-            <p className="info-value">{infoTexts.edu} </p>
+            <p className="info-key">{infoTexts.edukey}</p>
+            <p className="info-value">{infoTexts.edu}</p>
 
-            <p className="info-key">{infoTexts.rolekey} </p>
-            <p className="info-value">{infoTexts.role} </p>
+            <p className="info-key">{infoTexts.rolekey}</p>
+            <p className="info-value">{infoTexts.role}</p>
           </div>
         </div>
-        >
-        <div className="about-me">
+
+        <div className="about-me flex flex-col justify-center gap-5">
           <h3>{aboutTexts.title}</h3>
-          <p>{aboutTexts.p1} </p>
-          <p>{aboutTexts.p2} </p>
+          <p>{aboutTexts.p1}</p>
+          <p>{aboutTexts.p2}</p>
         </div>
       </div>
     </div>
