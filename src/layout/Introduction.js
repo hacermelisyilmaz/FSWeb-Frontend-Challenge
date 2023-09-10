901c5c32427968b91a62d72b82343eb379e1133b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-10 11:24:27 +0300

    Icons are added

--- src/layout/Introduction.js
+++ src/layout/Introduction.js
@@ -1,4 +1,8 @@
 import { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 import { TextContext } from "../context/TextProvider";
 import { ModeLangContext } from "../context/ModeLangProvider";
 
@@ -9,26 +13,48 @@ function Introduction() {
   const componentTexts = textData[lang].introduction;
 
   return (
-    <div className="Introduction">
-      <div id="intro-left">
-        <div id="greeting">
-          <h2>{componentTexts.greeting}</h2>
-          <div>
-            <h1>{componentTexts.p1}</h1>
-            <h1>{componentTexts.p2}</h1>
+    <div className="Introduction component bg-graybg">
+      <div className="w-4/5 mx-auto flex place-content-between">
+        <div id="intro-left">
+          <div id="greeting">
+            <p>{componentTexts.greeting}</p>
+            <div>
+              <h1>{componentTexts.p1}</h1>
+              <h1>{componentTexts.p2}</h1>
+            </div>
+          </div>
+          <div id="intro-footer">
+            <div id="icons" className="flex">
+              <a href="https://www.linkedin.com/in/hacermelisyilmaz/">
+                <FontAwesomeIcon
+                  icon={faLinkedinIn}
+                  size="2x"
+                  style={{ color: "#0a0a14" }}
+                />
+              </a>
+              <a href="https://github.com/hacermelisyilmaz">
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  size="2x"
+                  style={{ color: "#0a0a14" }}
+                />
+              </a>
+            </div>
+            <div id="intro-footer-text">
+              <p>{componentTexts.p3}</p>
+              <div className="flex flex-wrap items-center">
+                <p>{componentTexts.p4}</p>
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  style={{ color: "#000000" }}
+                />
+                <p>{componentTexts.p5}</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div id="info-footer">
-          {/* ikonlar buraya */}
-          <p>{componentTexts.p3}</p>
-          <div>
-            <p>{componentTexts.p4}</p>
-            {/* arrow ikonu */}
-            <p>{componentTexts.p5}</p>
-          </div>
-        </div>
+        <img src={componentTexts.image} />
       </div>
-      <img src={componentTexts.image} />
     </div>
   );
 }
